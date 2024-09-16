28593fe54ff4c5928ccf4e774a81ac5da4e22113
nicosaboonchi <nico.saboonchi@gmail.com>
2024-09-16 14:58:51 -0700

    Created tabs and changed wording

--- PhotoRenamer.py
+++ PhotoRenamer.py
@@ -13,7 +13,6 @@ from tkinter.filedialog import askdirectory
 import os
 import csv
 import shutil
-from HelperFunctions import timer
 import time
 import webbrowser
 
@@ -47,9 +46,6 @@ def getDestPath():
         global selected_dest
         selected_dest = dest_path
 
-# def get_total_rows(csv_file):
-#     with open(csv_file, "r") as file:
-#         return sum(1 for line in file)
 
 def get_total_photos(csv_file):
     total_photos = 0
@@ -64,13 +60,13 @@ def update_label(processed_rows, total_rows):
     progress_label.config(text=f"Processed {processed_rows} / {total_rows} photos")
 
 # checks to make sure all variables are satisfied then runs renaming
-@timer
 def run_script():
     if not (selected_csv and selected_source and selected_dest):
         messagebox.showerror(message="Please select CSV file, source directory, and destination directory.")
         return
 
     run.grid_forget()
+    help_button.grid_forget()
     progress_bar.grid(column=0, row=4, sticky=(W, E), padx=5, pady=10, columnspan=2)
     progress_label.grid(column=0, row=5, padx=5, pady=10, columnspan=2)
 
@@ -78,8 +74,6 @@ def run_script():
     progress_bar["maximum"] = total_photos
 
     time_start = time.time()
-
-    missing_photos = []
 
     with open(selected_csv, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
@@ -103,7 +97,6 @@ def run_script():
                         print(f"Successfully renamed {img} to {new_file_name}")
                     except FileNotFoundError:
                         print(f"{img} file not found")
-                        missing_photos.append(img)
 
                 processed_photos += 1
                 progress_bar["value"] = processed_photos
@@ -113,41 +106,48 @@ def run_script():
     time_end = time.time()
     total_time = time_end - time_start
 
-    messagebox.showinfo(message=f"Photos have been renamed with {len(missing_photos)} errors\nCompleted in {total_time:.2f} seconds" )
-
-    if missing_photos:
-        missing_photos_str = "\n".join(missing_photos)
-        messagebox.showwarning(message=f"The following files were not found:\n{missing_photos_str}")
+    messagebox.showinfo(message=f"Photos have been renamed in {total_time:.2f} seconds" )
 
     root.destroy()
 
 # GUI setup and creation
 root = Tk()
-root.title("Photo Rename")
+root.title("Photo Renamer")
 root.eval("tk::PlaceWindow . center")
 root.resizable(False, False)
 
+# Create tabs within the GUI for different departments
+notebook = ttk.Notebook(root)
+notebook.grid(column=0, row=0)
+
 # Creating widgets
-content = ttk.Frame(root, padding=10)
+fm_tab = ttk.Frame(notebook, padding=10)
+eas_tab = ttk.Frame(notebook, padding=10)
 
-summary = ttk.Label(content, text="This tool allows you to rename photos from fulcrum using a CSV file.\n" +
-                                  "Please enter the fulcrum asset CSV, the folder where the photos are located,\n" +
-                                  "and the folder where you want the renamed photos to be copied")
+notebook.add(fm_tab, text="FM Renamer")
+notebook.add(eas_tab, text="EAS Tab Coming Soon")
 
-csv_label = ttk.Label(content, text="Open CSV File")
-source_label = ttk.Label(content, text="Open Source Directory")
-dest_label = ttk.Label(content, text="Open Destination Path")
-csv_button = ttk.Button(content, text="Select CSV", command=getCSV)
-source_button = ttk.Button(content, text="Select Source Destination", command=getSourceDir)
-dest_button = ttk.Button(content, text="Select Destination Directory", command=getDestPath)
-run = ttk.Button(content, text="Rename Photos", command=run_script)
-progress_bar = ttk.Progressbar(content, orient="horizontal", mode="determinate", length=300)
-progress_label = ttk.Label(content, text="")
-help_button = ttk.Button(content, text="Help", command=lambda:webbrowser.open("https://github.com/nicosaboonchi/PhotoRename"))
+summary = ttk.Label(fm_tab, text="Welcome to the FM photo renamer. This app renames photos from Fulcrum using\n"+
+                                 "the barcode column from a Fulcrum CSV.\n\n" +
+                                 "Please select a Fulcrum CSV, the source directory where the photos are stored,\n"+
+                                 "and the destination directory where you want the photos to be copied to.\n\n" +
+                                 "Press the help button for more information and instructions.")
 
-# placement of gui and widgets
-content.grid(column=0, row=0)
-summary.grid(column=0, row=0, columnspan=3, padx=5, pady=10, sticky="w")
+# FM GUI Block ---------------------------------------------------------
+# Widget Initialization
+csv_label = ttk.Label(fm_tab, text="Open CSV File")
+source_label = ttk.Label(fm_tab, text="Open Source Directory")
+dest_label = ttk.Label(fm_tab, text="Open Destination Path")
+csv_button = ttk.Button(fm_tab, text="Select CSV", command=getCSV)
+source_button = ttk.Button(fm_tab, text="Select Source Destination", command=getSourceDir)
+dest_button = ttk.Button(fm_tab, text="Select Destination Directory", command=getDestPath)
+run = ttk.Button(fm_tab, text="Rename Photos", command=run_script)
+progress_bar = ttk.Progressbar(fm_tab, orient="horizontal", mode="determinate", length=300)
+progress_label = ttk.Label(fm_tab, text="")
+help_button = ttk.Button(fm_tab, text="Help", command=lambda:webbrowser.open("https://github.com/nicosaboonchi/PhotoRename"))
+
+# Placement of widgets
+summary.grid(column=0, row=0, columnspan=2, padx=5, pady=10, sticky="w")
 csv_label.grid(column=0, row=1, sticky="w", padx=5, pady=10)
 source_label.grid(column=0, row=2, sticky="w", padx=5, pady=10)
 dest_label.grid(column=0, row=3, sticky="w", padx=5, pady=10)
@@ -156,6 +156,7 @@ source_button.grid(column=1, row=2, sticky="e", padx=5, pady=10)
 dest_button.grid(column=1, sticky="e", padx=5, pady=10, row=3)
 run.grid(column=1, row=4, sticky="e", padx=5, pady=10)
 help_button.grid(column=0, row=4, sticky="w", padx=5, pady=10)
+# ---------------------------------------------------------------------------
 
 root.mainloop()
 
